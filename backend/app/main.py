5be7060bc5ca0d5a8ca3779e56393f0ca8a17400
mubinui <mubinui@users.noreply.github.com>
2026-09-11 22:14:27 +0600

    Version 1.5.0

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,7 +41,7 @@ def create_app() -> FastAPI:
     configure_logging()
     app = FastAPI(
         title=settings.project_name,
-        version=settings.release or "1.4.1",
+        version=settings.release or "1.5.0",
         docs_url="/docs" if not settings.is_production else None,
         redoc_url=None,
         openapi_url="/openapi.json",
